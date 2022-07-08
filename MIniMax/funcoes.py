5c287373e7c1a37df96b6ecb53d3bf6074160d83
Klesley Silva de Oliveira <69215676+klesleySilvaOliveira@users.noreply.github.com>
2022-07-07 21:13:38 -0300

    Minimax completo com AlfaBeta

--- MIniMax/funcoes.py
+++ MIniMax/funcoes.py
@@ -2,6 +2,10 @@
 from tabuleiro_velha import *
 import copy
 
+maxx = 0
+minn = 0
+
+#Confere se há algum espaço vazio na matriz. 1 para sim e 0 para não
 def verifica_vazio(tabuleiro):
 	for i in range(0, 3, 1):
 		for j in range(0, 3, 1):
@@ -9,9 +13,21 @@ def verifica_vazio(tabuleiro):
 				return 1 #tem vazio
 	return 0 #cheio
 
+#Adiciona o valor informado nas posições informadas
 def add_velha(tabuleiro, valor, x, y):
 	tabuleiro.matrix[x][y] = valor
 
+#REcebe 2 matrizes e retorna uma lista com as posições que diferem nas matrizes
+def tip(tabuleiro1, tabuleiro2):
+	lista = []
+	for i in range(0, 3, 1):
+		for j in range(0, 3, 1):
+			if tabuleiro1.matrix[i][j] != tabuleiro2.matrix[i][j]:
+				lista.append(i)
+				lista.append(j)
+	return lista
+
+#Confere se os valores informados estão no intervalo de disponibilidade da matriz informada
 def confere_validade(tabuleiro, x, y):
 	if x < 0 or y < 0 or x > 2 or y > 2:
 		return 0
@@ -19,6 +35,7 @@ def confere_validade(tabuleiro, x, y):
 		return 1
 	return 0
 
+#Imprime o tabuleiro de forma padronizada
 def printar_tabuleiro(tabuleiro):
 	print("┌───┬───┬───┐")
 	print(f"│ {tabuleiro.matrix[0][0]} │ {tabuleiro.matrix[0][1]} │ {tabuleiro.matrix[0][2]} │")
@@ -28,6 +45,7 @@ def printar_tabuleiro(tabuleiro):
 	print(f"│ {tabuleiro.matrix[2][0]} │ {tabuleiro.matrix[2][1]} │ {tabuleiro.matrix[2][2]} │")
 	print("└───┴───┴───┘")
 
+#Verifica as condições de término e retorna o vencedor ou empate
 def verificar_termino(tabuleiro):
 	# Verificar verticais
 	if tabuleiro.matrix[0][0] == tabuleiro.matrix[1][0] == tabuleiro.matrix[2][0] != " ":
@@ -57,6 +75,7 @@ def verificar_termino(tabuleiro):
 	else:
 		return "espaços vazios"
 
+#verifica a quantidade de cada elemento na matriz para definir de quem é a vez
 def verifica_vez(tabuleiro):
 	x = 0
 	o = 0
@@ -72,8 +91,14 @@ def verifica_vez(tabuleiro):
 		return "X"
 
 def maxi(tabuleiro):
+	#Soma quantas vezes MAXI foi realizado
+	global maxx
+	maxx += 1
+
+	#Verifica o termino e adiciona o valor na variável 'termino'
 	termino = verificar_termino(tabuleiro)
 
+	#REaliza testes de finalidade na variável teste, para finalizar a recursão retornando 1 para vitoria do MAX, -1 para vitória do MIN e 0 para empate
 	if termino == "x" or termino == "X":
 		return [tabuleiro, 1]
 	elif termino == "o" or termino == "O":
@@ -81,23 +106,30 @@ def maxi(tabuleiro):
 	elif termino == "empate":
 		return [tabuleiro, 0]
 
-	#melhor = -inf
+	#Cria as variáveis melhor e melhor_movimento, que contém o melhor resultado e o melhor movimento, respectivamente
 	melhor = -10
 	melhor_movimento = copy.deepcopy(tabuleiro)
 
 	for x in range(0, 3, 1):
 		for y in range(0, 3, 1):
+			#Faz uma cópia total do tabuleiro informado para uma variável que possa ser alterada
 			estado = copy.deepcopy(tabuleiro)
+			#Adiciona o movimento em cada um dos espaços disponíveis e chama a recursão de acordo com o movimento alcançado
 			if estado.matrix[x][y] == " ":
 				estado.matrix[x][y] = "X"
 				maximo = mini(estado)[1]
+				#Se o valor resgatado na recursão for maior que o valor presente na variável 'melhor', 'melhor' recebe o valor da recursão e 'melhor_movimento' recebe o estado copiado
 				if maximo > melhor:
 					melhor = maximo
 					melhor_movimento = copy.deepcopy(estado)
-	
+
+	#REtorna o melhor movimento e o resultado final projetado
 	return [melhor_movimento, melhor]
 
 def mini(tabuleiro):
+	#Soma quantas vezes MINI foi realizado
+	global minn
+	minn += 1
 	termino = verificar_termino(tabuleiro)
 
 	if termino == "x" or termino == "X":
@@ -124,8 +156,20 @@ def mini(tabuleiro):
 	return [melhor_movimento, melhor]
 
 def minimax(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
+	#REsgata as variáveis globais MAXX e MINN
+	global maxx
+	global minn
+
+	#Cria o vetor vazio que receberá as dicas
+	dica = []
+
+	#Verifica de quem é a vez e adiciona na variável
+	vez = verifica_vez(tabuleiro)
+
+	#Enquanto o tabuleiro possui espaços vazios...
 	while verifica_vazio(tabuleiro):
 
+		#Verifica término
 		termino = verificar_termino(tabuleiro)
 
 		if termino == "x" or termino == "X":
@@ -135,36 +179,66 @@ def minimax(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
 		elif termino == "empate":
 			return tabuleiro
 
-		vez = verifica_vez(tabuleiro)
+		#Imprime o tabuleiro atual e de quem é a vez
 		printar_tabuleiro(tabuleiro)
 		print("VEZ DO " + vez)
 
-		if vez == "X":
+		#Vez do X
+		if vez == "X":			
+			#Se a variável player1, informada no cabeçalho, for 1 (é jogador ativo)...
 			if player1:
+				#Declara 2 variáveis responsáveis pela posição informada pelo jogador
 				x = -1
 				y = -1
-				while x < 0 or y < 0 or x > 2 or y > 2:
-					x, y = map(int, input("Enter two values: ").split())
-					if not confere_validade(tabuleiro, x, y):
-						x = -1
+
+				#REcebe uma dica de jogada ao enviar o tabuleiro atual e o tabuleiro alcançado por 'maxi' e imprime a dica
+				dica = tip(tabuleiro, maxi(tabuleiro)[0])
+				print("Dica de melhor jogada: " + str(dica[0]) + " e " + str(dica[1]))
+
+				#Enquanto os valores x e y não forem válidos...
+				while not confere_validade(tabuleiro, x, y):
+					#REcebe valores separados por vírgula e adiciona às variáveis x e y
+					x, y = map(int, input("Informe as posições válidas desejadas (0 ~ 2) separadas por espaço: ").split())
+
+				#Adiciona a jogada informada pelo usuário no tabuleiro
 				add_velha(tabuleiro, "X", x, y)
+
+			#Caso não seja jogador ativo...
 			else:
+				#...IA irá jogar
 				tabuleiro = maxi(tabuleiro)[0]
+
+			#Altera a variável 'vez' para o valor 'O'
+			vez = "O"
+
+		#Vez do X
 		elif vez == "O":
+			#Se a variável player2, informada no cabeçalho, for 1 (é jogador ativo)...
 			if player2:
 				x = -1
 				y = -1
-				while x < 0 or y < 0 or x > 2 or y > 2:
-					x, y = map(int, input("Enter two values: ").split())
-					if not confere_validade(tabuleiro, x, y):
-						x = -1
+
+				#REcebe uma dica de jogada ao enviar o tabuleiro atual e o tabuleiro alcançado por 'mini' e imprime a dica
+				dica = tip(tabuleiro, mini(tabuleiro)[0])
+				print("Dica de melhor jogada: " + str(dica[0]) + " e " + str(dica[1]))
+
+				while not confere_validade(tabuleiro, x, y):
+					x, y = map(int, input("Informe as posições válidas desejadas (0 ~ 2) separadas por espaço: ").split())
+
 				add_velha(tabuleiro, "O", x, y)
 			else:
 				tabuleiro = mini(tabuleiro)[0]
 
-	return tabuleiro
+			#Altera a variável 'vez' para o valor 'X'
+			vez = "X"
 
-def maxi_alfa(tabuleiro):
+	#REtorna o tabuleiro finalizado e as quantidades iteradas de MAXX e MINN
+	return [tabuleiro, maxx, minn]
+
+def maxi_otm(tabuleiro):
+	global maxx
+	maxx += 1
+
 	termino = verificar_termino(tabuleiro)
 
 	if termino == "x" or termino == "X":
@@ -174,7 +248,6 @@ def maxi_alfa(tabuleiro):
 	elif termino == "empate":
 		return [tabuleiro, 0]
 
-	#melhor = -inf
 	melhor = -10
 	melhor_movimento = copy.deepcopy(tabuleiro)
 
@@ -183,16 +256,20 @@ def maxi_alfa(tabuleiro):
 			estado = copy.deepcopy(tabuleiro)
 			if estado.matrix[x][y] == " ":
 				estado.matrix[x][y] = "X"
-				maximo = mini_beta(estado)[1]
+				maximo = mini_otm(estado)[1]
 				if maximo > melhor:
 					melhor = maximo
 					melhor_movimento = copy.deepcopy(estado)
+				#Como 1 é o maior valor possível para 'maxi_otm', e o 'maximo' sempre será o primeiro maior número resgatado, na ocorrência de 1, o algoritmo já finaliza
 				if maximo == 1:
 					return [melhor_movimento, melhor]
 	
 	return [melhor_movimento, melhor]
 
-def mini_beta(tabuleiro):
+def mini_otm(tabuleiro):
+	global minn
+	minn += 1
+
 	termino = verificar_termino(tabuleiro)
 
 	if termino == "x" or termino == "X":
@@ -211,16 +288,23 @@ def mini_beta(tabuleiro):
 			estado = copy.deepcopy(tabuleiro)
 			if estado.matrix[x][y] == " ":
 				estado.matrix[x][y] = "O"
-				minimo = maxi_alfa(estado)[1]
+				minimo = maxi_otm(estado)[1]
 				if minimo < melhor:
 					melhor = minimo
 					melhor_movimento = copy.deepcopy(estado)
+				#Como -1 é o menor valor possível para 'mini_otm', e o 'minimo' sempre será o primeiro menor número resgatado, na ocorrência de -1, o algoritmo já finaliza
 				if minimo == -1:
 					return [melhor_movimento, melhor]
 	
 	return [melhor_movimento, melhor]
 
-def minimax_ab(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
+def minimax_otm(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
+	global maxx
+	global minn
+	dica = []
+
+	vez = verifica_vez(tabuleiro)
+
 	while verifica_vazio(tabuleiro):
 
 		termino = verificar_termino(tabuleiro)
@@ -232,7 +316,6 @@ def minimax_ab(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
 		elif termino == "empate":
 			return tabuleiro
 
-		vez = verifica_vez(tabuleiro)
 		printar_tabuleiro(tabuleiro)
 		print("VEZ DO " + vez)
 
@@ -240,23 +323,169 @@ def minimax_ab(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
 			if player1:
 				x = -1
 				y = -1
-				while x < 0 or y < 0 or x > 2 or y > 2:
-					x, y = map(int, input("Enter two values: ").split())
-					if not confere_validade(tabuleiro, x, y):
-						x = -1
+
+				dica = tip(tabuleiro, maxi_otm(tabuleiro)[0])
+				print("Dica de melhor jogada: " + str(dica[0]) + " e " + str(dica[1]))
+
+				while not confere_validade(tabuleiro, x, y):
+					x, y = map(int, input("Informe as posições válidas desejadas (0 ~ 2) separadas por espaço: ").split())
+				
 				add_velha(tabuleiro, "X", x, y)
 			else:
-				tabuleiro = maxi_alfa(tabuleiro)[0]
+				tabuleiro = maxi_otm(tabuleiro)[0]
+
+			vez = "O"
+
 		elif vez == "O":
 			if player2:
 				x = -1
 				y = -1
-				while x < 0 or y < 0 or x > 2 or y > 2:
-					x, y = map(int, input("Enter two values: ").split())
-					if not confere_validade(tabuleiro, x, y):
-						x = -1
-				add_velha(tabuleiro, "O", x, y)
-			else:
-				tabuleiro = mini_beta(tabuleiro)[0]
 
-	return tabuleiro
+				dica = tip(tabuleiro, mini_otm(tabuleiro)[0])
+				print("Dica de melhor jogada: " + str(dica[0]) + " e " + str(dica[1]))
+
+				while not confere_validade(tabuleiro, x, y):
+					x, y = map(int, input("Informe as posições válidas desejadas (0 ~ 2) separadas por espaço: ").split())
+				
+				add_velha(tabuleiro, "O", x, y)
+
+			else:
+				tabuleiro = mini_otm(tabuleiro)[0]
+
+			vez = "X"
+
+	return [tabuleiro, maxx, minn]
+
+def maxi_alfa(tabuleiro, alfa, beta):
+	global maxx
+	maxx += 1
+
+	termino = verificar_termino(tabuleiro)
+
+	if termino == "x" or termino == "X":
+		return [tabuleiro, 1]
+	elif termino == "o" or termino == "O":
+		return [tabuleiro, -1]
+	elif termino == "empate":
+		return [tabuleiro, 0]
+
+	melhor = -10
+	melhor_movimento = copy.deepcopy(tabuleiro)
+
+	for x in range(0, 3, 1):
+		for y in range(0, 3, 1):
+			estado = copy.deepcopy(tabuleiro)
+			if estado.matrix[x][y] == " ":
+				estado.matrix[x][y] = "X"
+				maximo = mini_beta(estado, alfa, beta)[1]
+				
+				if maximo > melhor:
+					melhor = maximo
+					melhor_movimento = copy.deepcopy(estado)
+				
+				#Se o valor calculado for maior ou igual a beta, finaliza o algoritmo
+				if maximo >= beta:
+					return [melhor_movimento, melhor]
+				
+				#Se o melhor valor calculado for maior que o alfa, alfa recebe o melhor valor
+				if melhor > alfa:
+					alfa = melhor
+	
+	return [melhor_movimento, melhor]
+
+def mini_beta(tabuleiro, alfa, beta):
+	global minn
+	minn += 1
+
+	termino = verificar_termino(tabuleiro)
+
+	if termino == "x" or termino == "X":
+		return [tabuleiro, 1]
+	elif termino == "o" or termino == "O":
+		return [tabuleiro, -1]
+	elif termino == "empate":
+		return [tabuleiro, 0]
+
+	melhor = 10
+	melhor_movimento = copy.deepcopy(tabuleiro)
+
+	for x in range(0, 3, 1):
+		for y in range(0, 3, 1):
+			estado = copy.deepcopy(tabuleiro)
+			if estado.matrix[x][y] == " ":
+				estado.matrix[x][y] = "O"
+				minimo = maxi_alfa(estado, alfa, beta)[1]
+
+				if minimo < melhor:
+					melhor = minimo
+					melhor_movimento = copy.deepcopy(estado)
+
+				#Se o valor calculado for menor ou igual a alfa, finaliza o algoritmo
+				if minimo <= alfa:
+					return [melhor_movimento, melhor]
+
+				#Se o melhor valor calculado for menor que o beta, beta recebe o melhor valor
+				if melhor < beta:
+					beta = melhor
+	return [melhor_movimento, melhor]
+
+def minimax_alfa_beta(tabuleiro, player1, player2): #player = 1 for player 0 for CPU
+	global maxx
+	global minn
+
+	dica = []
+
+	vez = verifica_vez(tabuleiro)
+
+	while verifica_vazio(tabuleiro):
+
+		termino = verificar_termino(tabuleiro)
+
+		if termino == "x" or termino == "X":
+			return tabuleiro
+		elif termino == "o" or termino == "O":
+			return tabuleiro
+		elif termino == "empate":
+			return tabuleiro
+
+		
+		printar_tabuleiro(tabuleiro)
+		print("VEZ DO " + vez)
+
+		if vez == "X":
+			if player1:
+				x = -1
+				y = -1
+
+				dica = tip(tabuleiro, maxi_alfa(tabuleiro, -10, 10)[0])
+				print("Dica de melhor jogada: " + str(dica[0]) + " e " + str(dica[1]))
+
+				while not confere_validade(tabuleiro, x, y):
+					x, y = map(int, input("Informe as posições válidas desejadas (0 ~ 2) separadas por espaço: ").split())
+
+				add_velha(tabuleiro, "X", x, y)
+
+			else:
+				tabuleiro = maxi_alfa(tabuleiro, -10, 10)[0]
+
+			vez = "O"
+
+		elif vez == "O":
+			if player2:
+				x = -1
+				y = -1
+
+				dica = tip(tabuleiro, mini_beta(tabuleiro, -10, 10)[0])
+				print("Dica de melhor jogada: " + str(dica[0]) + " e " + str(dica[1]))
+
+				while not confere_validade(tabuleiro, x, y):
+					x, y = map(int, input("Informe as posições válidas desejadas (0 ~ 2) separadas por espaço: ").split())
+
+				add_velha(tabuleiro, "O", x, y)
+
+			else:
+				tabuleiro = mini_beta(tabuleiro, -10, 10)[0]
+
+			vez = "X"
+
+	return [tabuleiro, maxx, minn]
--- MIniMax/main.py
+++ MIniMax/main.py
@@ -1,9 +1,41 @@
 # -*- coding: utf-8 -*-
 from tabuleiro_velha import *
 from funcoes import *
+import time
 
 tabuleiro = velha()
+fim_de_jogo = []
+jogo = -1
+jogador1 = -1
+jogador2 = -1
 
-tabuleiro = minimax_ab(tabuleiro, 0, 0)
-printar_tabuleiro(tabuleiro)
-print("Houve vencedor? "+verificar_termino(tabuleiro)) #printa quem ganhou X ou O...
+while(jogo < 0 or jogo > 2):
+	jogo = int(input("> 0 - MiniMax comum\n> 1 - MiniMax otimizado\n> 2 - MiniMax Alfa-Beta\n> Informe a opção: "))
+
+while(jogador1 < 0 or jogador1 > 1 or jogador2 < 0 or jogador2 > 1):
+	print("\nJogador X:\n")
+	jogador1 = int(input("> 0 - IA\n> 1 - JOGADOR\n> Informe a opção: "))
+	print("\nJogador O:\n")
+	jogador2 = int(input("> 0 - IA\n> 1 - JOGADOR\n> Informe a opção: "))
+
+
+# get the start time
+st = time.time()
+
+if jogo == 0:
+	fim_de_jogo = minimax(tabuleiro, jogador1, jogador2)
+elif jogo == 1:
+	fim_de_jogo = minimax_otm(tabuleiro, jogador1, jogador2)
+else:
+	fim_de_jogo = minimax_alfa_beta(tabuleiro, jogador1, jogador2)
+
+printar_tabuleiro(fim_de_jogo[0])
+print("Houve vencedor? " + verificar_termino(fim_de_jogo[0])) #printa quem ganhou X ou O...
+print("Foram realizadas ", fim_de_jogo[1]," iterações de MAX e ", fim_de_jogo[2], " iterações de MIN.")
+
+# get the end time
+et = time.time()
+
+# get the execution time
+elapsed_time = et - st
+print('Tempo de execução:', elapsed_time, 'segundos')
